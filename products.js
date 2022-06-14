f1cfe81aa7458382a0aae4070e685963ea9fe349
danny kyman <danny.kyman@gmail.com>
2022-06-14 09:45:52 -0700

    Added initial layout

---
+++ products.js
@@ -0,0 +1,44 @@
+const products = [
+  {
+    id: 0,
+    name: "Item 1",
+    price: 29.99,
+    instock: 100,
+    imgSrc: "./icons/rocket.png",
+  },
+  {
+    id: 1,
+    name: "Item 2",
+    price: 24.99,
+    instock: 43,
+    imgSrc: "./icons/rocket.png",
+  },
+  {
+    id: 2,
+    name: "Item 3",
+    price: 19.99,
+    instock: 10,
+    imgSrc: "./icons/rocket.png",
+  },
+  {
+    id: 3,
+    name: "Item 4",
+    price: 25.99,
+    instock: 5,
+    imgSrc: "./icons/rocket.png",
+  },
+  {
+    id: 4,
+    name: "Item 5",
+    price: 29.99,
+    instock: 4,
+    imgSrc: "./icons/rocket.png",
+  },
+  {
+    id: 5,
+    name: "Item 6",
+    price: 39.99,
+    instock: 40,
+    imgSrc: "./icons/rocket.png",
+  },
+];
